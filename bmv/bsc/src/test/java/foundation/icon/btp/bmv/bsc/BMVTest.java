65ceacc4fa207aee57d7b0fcc0c96fb778ae03e7
EunKi Yu <yueunki@iconloop.com>
2023-03-08 20:32:28 +0900

    Add test cases for bsc bmv

---
+++ bmv/bsc/src/test/java/foundation/icon/btp/bmv/bsc/BMVTest.java
@@ -0,0 +1,86 @@
+package foundation.icon.btp.bmv.bsc;
+
+import com.iconloop.score.test.Account;
+import com.iconloop.score.test.Score;
+import com.iconloop.score.test.ServiceManager;
+import com.iconloop.score.test.TestBase;
+import foundation.icon.btp.lib.BMVStatus;
+import foundation.icon.btp.lib.BTPAddress;
+import foundation.icon.score.util.StringUtil;
+import org.junit.jupiter.api.*;
+
+import java.math.BigInteger;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BMVTest extends TestBase {
+    static final ServiceManager sm = getServiceManager();
+    static final Account BMC = sm.createAccount(Integer.MAX_VALUE);
+    static final BTPAddress BMC_BTP_ADDR = BTPAddress.parse("btp://0x1.icon/cx123");
+
+    public static Score deployBmv(DataSource.ConstructorParams params) throws Exception {
+        return sm.deploy(sm.createAccount(), BTPMessageVerifier.class,
+                params.getChainId(), params.getEpoch(), StringUtil.hexToBytes(params.getHeader()),
+                params.getRecents().toArray(EthAddress[]::new),
+                params.getValidators().toArray(EthAddress[]::new));
+    }
+
+    public static void handleRelayMessageTest(DataSource.Case c, Score bmv, String prev) {
+        System.out.println("case: " + c.getDescription());
+        for (DataSource.Case.Phase p : c.getPhases()) {
+            System.out.println("phase: " + p.getDescription());
+            byte[] relayMsg = StringUtil.hexToBytes(p.getInput());
+            List<String> messages = p.getMessages();
+            byte[][] ret = (byte[][]) sm.call(BMC, BigInteger.ZERO, bmv.getAddress(), "handleRelayMessage",
+                    BMC_BTP_ADDR.toString(), prev, BigInteger.valueOf(0), relayMsg);
+
+            if (messages.size() > 0) {
+                assertEquals(messages.size(), ret.length);
+                for (int i=0; i<messages.size(); i++) {
+                    assertEquals(messages.get(i), new String(ret[i]));
+                }
+            }
+            BMVStatus status = bmv.call(BMVStatus.class, "getStatus");
+            assertEquals(p.getStatus().getHeight(), status.getHeight());
+        }
+    }
+
+    public static class MainNetBMVTest extends TestBase {
+        private static DataSource data = DataSource.loadDataSource("mainnet.json");
+        @TestFactory
+        public Collection<DynamicTest> handleRelayMessageTests() {
+            DataSource.ConstructorParams params = data.getParams();
+            List<DynamicTest> t = new ArrayList<>();
+            for (DataSource.Case c : data.getCases()) {
+                t.add(DynamicTest.dynamicTest(c.getDescription(),
+                        () -> {
+                            Score bmv = deployBmv(params);
+                            handleRelayMessageTest(c, bmv, "");
+                        }
+                ));
+            }
+            return t;
+        }
+    }
+
+    public static class OneValidatorBMVTest extends TestBase {
+        private static DataSource data = DataSource.loadDataSource("privnet.json");;
+        private static final BTPAddress PREV_BMC = BTPAddress.parse("btp://0x1.eth/0xD2F04942FF92709ED9d41988D161710D18d7f1FE");
+
+        @TestFactory
+        public Collection<DynamicTest> handleRelayMessageTests() {
+            DataSource.ConstructorParams params = data.getParams();
+            List<DynamicTest> t = new ArrayList<>();
+            for (DataSource.Case c : data.getCases()) {
+                t.add(DynamicTest.dynamicTest(c.getDescription(),
+                        () -> {
+                            Score bmv = deployBmv(params);
+                            handleRelayMessageTest(c, bmv, PREV_BMC.toString());
+                        }
+                ));
+            }
+            return t;
+        }
+    }
+}
